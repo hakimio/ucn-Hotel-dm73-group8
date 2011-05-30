400649624a4fc6d7e05f0bae6dd85083a8e0c7e0
Tomas Rimkus <sandshrew@gmail.com>
2011-05-30 17:13:11 +0200

    arrivalDate is no longer checked if it's in the past in the Booking class. This check should be in the controller.

--- Hotel/src/hotel/core/Booking.java
+++ Hotel/src/hotel/core/Booking.java
@@ -4,29 +4,32 @@ import java.util.Date;
 
 public class Booking
 {
+    private int id;
     private Room room;
     private Guest guest;
     private Date arrivalDate, leavingDate;
     private int discount;
     
-    public Booking(Room room, Guest guest, Date arrivalDate, Date leavingDate,
+    public Booking(int id, Room room, Guest guest, Date arrivalDate, Date leavingDate,
             int discount)
     {
-        if (arrivalDate.compareTo(new Date()) < 0)
-            throw new IllegalArgumentException("Arrival date can not be in the"+
-                    " past.");
         if (arrivalDate.compareTo(leavingDate) == 0)
             throw new IllegalArgumentException("Arrival date can not be the " +
                     "same as leaving date");
         if(leavingDate.compareTo(arrivalDate) < 0)
             throw new IllegalArgumentException("Leaving date can not be before"+
                     " arrival date.");
-        
+        this.id = id;
         this.room = room;
         this.guest = guest;
         this.discount = discount;
         this.arrivalDate = arrivalDate;
         this.leavingDate = leavingDate;
+    }
+    
+    public int getId()
+    {
+        return id;
     }
     
     public void setLeavingDate(Date leavingDate)
