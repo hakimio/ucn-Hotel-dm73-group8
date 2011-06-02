8742b5cb315b88686b6b5c0b552205d7fdedcd65
Tomas Rimkus <sandshrew@gmail.com>
2011-06-02 22:14:41 +0200

    Added new addBooking and editBooking methods to BookingCtrl class.

--- Hotel/src/hotel/controller/BookingCtrl.java
+++ Hotel/src/hotel/controller/BookingCtrl.java
@@ -74,6 +74,119 @@ public class BookingCtrl
         bookingDB.insertBooking(booking, hotelName);
     }
     
+    public void addBooking(Guest guest, int nrOfGuests, Date arrivalDate, 
+            Date leavingDate, int discount)
+    {
+        if (arrivalDate.before(new Date()))
+            throw new IllegalStateException("Arrival date can not be in "
+                    + "the past.");
+        
+        RoomCtrl roomCtrl = new RoomCtrl(hotelName);
+        ArrayList<Room> roomsToCheck = new ArrayList<Room>();
+        Room foundRoom = null;
+        
+        for(int i = 0; i < roomCtrl.getRoomCount(); i++)
+        {
+            Room room = roomCtrl.getRoomById(i);
+            if (room.getNrOfBedrooms() == nrOfGuests)
+                roomsToCheck.add(room);
+        }
+        
+        for (int i = 0; i < roomsToCheck.size(); i++)
+        {
+            Room room = roomsToCheck.get(i);
+            if (isRoomAvailable(room, arrivalDate, leavingDate))
+            {
+                foundRoom = room;
+                break;
+            }
+        }
+        
+        if (foundRoom == null)
+            throw new IllegalStateException("There are no available rooms that"
+                    + " can hold the specified number of guests.");
+        
+        double totalPrice = foundRoom.getCost();
+        ExpenseCtrl expenseCtrl = new ExpenseCtrl(guest.getName());
+        
+        totalPrice += expenseCtrl.getTotalExpenses();
+        
+        if (discount > totalPrice)
+            throw new IllegalStateException("Discount can not be higher than"
+                    + " total price.");
+        int id = idGenerator.getNextId();
+        
+        Booking booking = new Booking(id, foundRoom, guest, arrivalDate, 
+                leavingDate, discount);
+        bookings.add(booking);
+        bookingDB.insertBooking(booking, hotelName);
+    }
+    
+    public void editBooking(int id, Guest guest, int nrOfGuests, 
+            Date arrivalDate, Date leavingDate, int discount)
+    {
+        RoomCtrl roomCtrl = new RoomCtrl(hotelName);
+        ArrayList<Room> roomsToCheck = new ArrayList<Room>();
+        Room foundRoom = null;
+        
+        for(int i = 0; i < roomCtrl.getRoomCount(); i++)
+        {
+            Room room = roomCtrl.getRoomById(i);
+            if (room.getNrOfBedrooms() == nrOfGuests)
+                roomsToCheck.add(room);
+        }
+        
+        for (int i = 0; i < roomsToCheck.size(); i++)
+        {
+            Room room = roomsToCheck.get(i);
+            if (isRoomAvailable(room, arrivalDate, leavingDate))
+            {
+                foundRoom = room;
+                break;
+            }
+        }
+        
+        if (foundRoom == null)
+            throw new IllegalStateException("There are no available rooms that"
+                    + " can hold the specified number of guests.");
+        
+        double totalPrice = foundRoom.getCost();
+        ExpenseCtrl expenseCtrl = new ExpenseCtrl(guest.getName());
+        
+        totalPrice += expenseCtrl.getTotalExpenses();
+        
+        if (discount > totalPrice)
+            throw new IllegalStateException("Discount can not be higher than"
+                    + " total price.");
+        
+        int bookingId = bookings.get(id).getId();
+        Booking booking = new Booking(bookingId, foundRoom, guest, arrivalDate, 
+                leavingDate, discount);
+        bookingDB.updateBooking(booking, hotelName);
+        bookings.set(id, booking);
+    }
+    
+    private boolean isRoomAvailable(Room room, Date arrivalDate,
+            Date leavingDate)
+    {
+        Booking[] bookingsToCheck = getBookingsByRoom(room);
+        
+        for (int i = 0; i < bookingsToCheck.length; i++)
+        {
+            Booking booking = bookingsToCheck[i];
+            
+            if (arrivalDate.after(booking.getArrivalDate()) && 
+                    arrivalDate.before(booking.getLeavingDate()))
+                return false;
+            
+            if(leavingDate.after(booking.getArrivalDate()) && 
+                    leavingDate.before(booking.getLeavingDate()))
+                return false;
+        }
+        
+        return true;
+    }
+    
     public void editBooking(int id, Room room, Guest guest, Date arrivalDate, 
             Date leavingDate, int discount)
     {
